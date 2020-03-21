e5dea963790117b53317673566c6eb952193a891
Keith McNulty <keith.mcnulty@gmail.com>
2020-03-21 20:02:00 +0000

    Edgelists

--- edgelist_creation/generate_edgelist.R
+++ edgelist_creation/generate_edgelist.R
@@ -1,6 +1,8 @@
 library(dplyr)
-source('scrape_friends.R')
-source('unique_pairs.R')
+source('edgelist_creation/scrape_friends.R')
+source('edgelist_creation/unique_pairs.R')
+
+# iterate through each episodes - pass no data if an error occurs (ie episode doesn't exist)
 
 raw_results <- data.frame(season = integer(), from = character(), to = character(), stringsAsFactors = FALSE)
 
@@ -8,10 +10,12 @@ for (season in 1:10) {
   
   season_results <- data.frame(from = character(), to = character(), stringsAsFactors = FALSE)
   
-  for (episode in 1:25) {
+  # no season has more than 25 episodes
+  for (episode in 1:25) { 
     
     message(paste("Scraping Season", season, "Episode", episode))
     
+    # pass empty df if scrape fails
     scrape <- tryCatch(
       scrape_friends(season, episode),
       error = function(e) {data.frame(from = character(), to = character(), stringsAsFactors = FALSE)}
@@ -45,13 +49,14 @@ for (season in 1:10) {
 }
 
 
-
+# order pairs alphabetically to deal with different orderings
 for (i in 1: nrow(raw_results)) {
   
   raw_results[i, c("from", "to")] <- sort(raw_results[i, c("from", "to")])
   
 }
 
+# add up scenes to form season weights
 edges <- raw_results %>% 
   dplyr::count(season, from, to, name = "weight")
 
@@ -59,4 +64,21 @@ if (!dir.exists("data")) {
   dir.create("data")
 }
 
-saveRDS(edges, "data/friends_edgelist.RDS")
+saveRDS(edges, "data/friends_edgelist_by_season.RDS")
+write.csv(edges, "data/friends_edgelist_by_season.csv", row.names = FALSE)
+
+# add up over the entire series
+edges <- as.data.frame(edges)
+
+for (i in 1:nrow(edges)) {
+  
+  edges[i, c("from", "to")] <- sort(edges[i, c("from", "to")])
+  
+}
+
+full_series <- edges %>% 
+  dplyr::group_by(from, to) %>% 
+  dplyr::summarise(weight = sum(weight))
+
+saveRDS(full_series, "data/friends_full_series_edgelist.RDS")
+write.csv(full_series, "data/friends_full_series_edgelist.csv", row.names = FALSE)
--- edgelist_creation/scrape_friends.R
+++ edgelist_creation/scrape_friends.R
@@ -10,6 +10,8 @@ library(stringr)
 
 scrape_friends <- function(season = 1, episode = 1) {
   
+  # some episodes are double episodes
+  
   if (season == 2 & episode == 12) {
     url_string <- "https://fangj.github.io/friends/season/0212-0213.html"
   } else if (season == 6 & episode == 15) {
@@ -25,41 +27,63 @@ scrape_friends <- function(season = 1, episode = 1) {
                          ".html")
   }
   
-  nodes <- xml2::read_html(url_string) %>% 
-    rvest::html_nodes("p") %>% 
-    xml2::as_list() %>% 
-    unlist() %>% 
-    as.vector()
+  # general html for seasons 1:9 is different from season 10
   
-  nodes <- ifelse(grepl("Scene:", nodes), "New Scene", nodes)
-
-  if (season == 10) {
-    nodes <- ifelse(nodes != "New Scene", 
-                    stringr::str_extract(nodes, ".+?(?=:)"),
-                    nodes)
-  } else if (season == 2 & episode %in% 3:25) {
-    nodes <- ifelse(nodes != "New Scene",  
-                    stringr::str_extract(nodes, "[[[:upper:]][[:punct:]]\\s]+(?=:)") %>% 
-                      tolower() %>% 
-                      tools::toTitleCase(),
-                    nodes)
+  if (season %in% 1:9) {
+    nodes <- xml2::read_html(url_string) %>% 
+      xml2::as_list() %>% 
+      unlist()
+    
+    nodes <- ifelse(grepl("Scene:", nodes), "New Scene", nodes) # mark scene boundaries
+    
+    # season 2 has some weirdly formatted episodes
+    if (season == 2 & episode %in% 3:25) {
+      nodes <- ifelse(nodes != "New Scene",  
+                      stringr::str_extract(nodes, "[[[:upper:]][[:punct:]]\\s]+(?=:)") %>% # look for caps preceeding a colon
+                        tolower() %>% 
+                        tools::toTitleCase(),
+                      nodes)
+    } else {
+      nodes <- ifelse(nodes != "New Scene", 
+                      stringr::str_extract(nodes, ".+?(?=:)"), #look for anything precedeing a colon
+                      nodes)
+    }
+    
   } else {
+    # season 10
+    nodes <- xml2::read_html(url_string) %>% 
+      rvest::html_nodes("p") %>% 
+      rvest::html_text() # anything in paragraph tags
+    
+    nodes <- ifelse(grepl("Scene:", nodes), "New Scene", nodes)
+    
     nodes <- ifelse(nodes != "New Scene", 
-                    stringr::str_extract(nodes, ".+?(?=:)"),
+                    stringr::str_extract(nodes, ".+?(?=:)"), # anything preceding a colon
                     nodes)
+    
   }
-
-
   
-  nodes <- nodes[!grepl("/| and |All|Nothing|&", nodes)]
+
+  # manual leaveouts and replacements - gets us 98% of the way I reckon 
+  nodes <- nodes[!is.na(nodes)]
+  nodes <- trimws(nodes)
+  nodes <- nodes[!grepl("/| and |all|everybody|&|by|position|aired|both|,|from|at 8|end|time|commercial|\\(|\\[|letters| to |it's|it was|kudrow|perry|cox|aniston|schwimmer|leblanc|look|could|walks|everyone", tolower(nodes))]
   nodes <- nodes[nchar(nodes) < 20]
+  nodes <- nodes[!grepl("^[a-z]|^[0-9]|^[[:punct:]]", nodes)]
   nodes <- gsub("<b>|\n", "", nodes)
+  nodes <- gsub("\u0092", "'", nodes)
+  nodes <- gsub("Mr.Heckles", "Mr. Heckles", nodes)
   nodes <- gsub("father", "dad", nodes)
   nodes <- gsub("mother", "mom", nodes)
+  nodes <- ifelse(nodes == "r Zelner", "Mr Zelner", nodes)
+  nodes <- ifelse(nodes == "r Zelner", "Mr Zelner", nodes)
+  nodes <- ifelse(nodes == "Mnca", "Monica", nodes)
   nodes <- ifelse(nodes == "Phoe", "Phoebe", nodes)
   nodes <- ifelse(nodes == "Rach", "Rachel", nodes)
   nodes <- ifelse(nodes == "Chan", "Chandler", nodes)
   
+  
+  # number each scene
   scene_count <- c()
   
   for (i in 1:length(nodes)) {
@@ -69,7 +93,8 @@ scrape_friends <- function(season = 1, episode = 1) {
   data.frame(episode = episode, scene = scene_count, character = nodes) %>% 
     dplyr::filter(character != "New Scene") %>% 
     dplyr::distinct(episode, scene, character) %>% 
-    dplyr::mutate(scene = scene - min(scene) + 1)
+    dplyr::mutate(scene = scene - min(scene) + 1, # set first scene number to 1
+                  character = character %>% tolower() %>% tools::toTitleCase()) # takes care of caps mistyping
   
   
 }
